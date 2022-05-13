6b209b2ad7f702705c2b1d5eca73c376160c85f1
javi <javierdelbas@gmail.com>
2022-05-13 10:06:58 +0200

    Creaada la entidad Maquina

---
+++ src/Entity/Maquina.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table()
+ */
+
+class Maquina
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     * @var int
+     */
+    private $numeroMaquina;
+}
