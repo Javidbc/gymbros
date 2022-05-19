5e882f470c23a244b18bbd05e6be8d94d6da418e
javi <javierdelbas@gmail.com>
2022-05-19 10:14:08 +0200

    Creado el controlador para la pagina principal

---
+++ src/Controller/PrincipalController.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PrincipalController extends AbstractController
+{
+    /**
+     * @Route ("/" , name="principal")
+     */
+    public function principal(): Response
+    {
+        return $this->render('base.html.twig');
+    }
+}
