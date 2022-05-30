dfd54c5a66e2e61e13c8d9ebd00cf04330bebd53
javi <javierdelbas@gmail.com>
2022-05-30 16:12:34 +0200

    Creado el repositiorio de usuario con la funcion para ver todos los usuarios

---
+++ src/Repository/UsuarioRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Repository;
+
+
+use App\Entity\Usuario;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+class UsuarioRepository  extends  ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Usuario::class);
+    }
+
+    public function verUsuarios() : array
+    {
+        return $this
+            ->getEntityManager()
+            ->createQuery("SELECT u FROM App\\Entity\\Usuario u ORDER BY u.apellidos DESC")
+            ->getResult();
+    }
+}
